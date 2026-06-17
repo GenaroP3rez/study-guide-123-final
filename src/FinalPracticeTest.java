43312f9e12675f4a272549ec289913e7970d4ff7
lilPunny <166659824+GenaroP3rez@users.noreply.github.com>
2026-06-17 16:51:59 -0700

    syntax and J unites done, syntax works

--- src/FinalPracticeTest.java
+++ src/FinalPracticeTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.Test;
 
@@ -39,6 +40,15 @@ public class FinalPracticeTest {
                 new ListNode(20));
 
         assertEquals(20, FinalPractice.oddIndexSum(list));
+    }
+
+    @Test
+    void testOddIndexSum_threeNodes() {
+        ListNode list = new ListNode(5,
+                new ListNode(10,
+                new ListNode(15)));
+
+        assertEquals(10, FinalPractice.oddIndexSum(list));
     }
 
     @Test
@@ -84,5 +94,52 @@ public class FinalPracticeTest {
                 new ListNode(0))));
 
         assertEquals(0, FinalPractice.oddIndexSum(list));
+    }
+
+    @Test
+    void testOddIndexSum_evenIndexesAreIgnored() {
+        ListNode list = new ListNode(100,
+                new ListNode(1,
+                new ListNode(200,
+                new ListNode(2,
+                new ListNode(300,
+                new ListNode(3))))));
+
+        assertEquals(6, FinalPractice.oddIndexSum(list));
+    }
+
+    @Test
+    void testOddIndexSum_valuesCancelEachOther() {
+        ListNode list = new ListNode(50,
+                new ListNode(10,
+                new ListNode(60,
+                new ListNode(-10))));
+
+        assertEquals(0, FinalPractice.oddIndexSum(list));
+    }
+
+    @Test
+    void testOddIndexSum_largeValues() {
+        ListNode list = new ListNode(1,
+                new ListNode(1_000_000,
+                new ListNode(2,
+                new ListNode(2_000_000))));
+
+        assertEquals(3_000_000, FinalPractice.oddIndexSum(list));
+    }
+
+    @Test
+    void testOddIndexSum_doesNotChangeList() {
+        ListNode third = new ListNode(15);
+        ListNode second = new ListNode(10, third);
+        ListNode list = new ListNode(5, second);
+
+        FinalPractice.oddIndexSum(list);
+
+        assertEquals(5, list.data);
+        assertSame(second, list.next);
+        assertEquals(10, list.next.data);
+        assertSame(third, list.next.next);
+        assertEquals(15, list.next.next.data);
     }
 }
